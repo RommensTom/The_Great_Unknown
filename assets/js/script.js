2148e7f2c047db7955d97d1a787e7878279a5b1a
TomRommens <tom.rommens@student.howest.be>
2018-05-26 22:04:48 +0200

    Local storage Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,6 @@ let countries = [];
 
 
 
-
-
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//         .register('./service-worker.js', {scope : './../'})
-//         .then(function (registration) {
-//             console.log("Service Worker Registered", registration);
-//         })
-//         .catch(function (err) {
-//             console.log("Service Worker Failed to Register", err);
-//         })
-// }
 if (navigator.serviceWorker.controller) {
     console.log('[PWA Builder] active service worker found, no need to register')
 } else {
@@ -44,9 +32,14 @@ let giveRandomCountry = function () {
         .then(response => response.json())
         .then(res => {
             console.log(res);
+            let landeke = {};
+            landeke.naam = res[0].name;
+            landeke.capital = res[0].capital;
+            landeke.population = res[0].population;
+            landeke.region = res[0].region;
             console.log(res[0].name);
             $('#Country').append("The great " + res[0].name);
-            saveCountry(res[0].name);
+            saveCountry(landeke);
             $('#name').append(res[0].name);
             $('#capital').append(res[0].capital);
             $('#flag').append("<img src=' " + res[0].flag + " ' alt='Country flag'>");
@@ -103,7 +96,10 @@ let displayCountries = function () {
     if(numberOfRandomCountries > 0){
         let list = '<ul>';
         for(let i =0; i < numberOfRandomCountries; i++) {
-            list += '<li> - '+ countries[i] + '</li>';
+            list += '<section id="historyList"> <h3>'+ countries[i].naam +'</h3>' +
+                '<li> ==> Capital: '+ countries[i].capital + '</li>' +
+                '<li> ==> Region: '+ countries[i].region + '</li>' +
+                '<li> ==> Population: '+ countries[i].population + '</li></section>';
         }
 
         list += '</ul>';
@@ -120,85 +116,3 @@ $('document').ready(function () {
     }
 
 });
-// var toonQuote = function (e) {
-//     e.preventDefault();
-//     $('#quote').empty();
-//     $.ajax({
-//         type: "GET", //-> http werkwoord
-//         url: 'https://restcountries.eu/rest/v2/all'
-//
-//     }).done(function (data, textStatus, jqXHR) {
-//         var zin = data.value;
-//         $('#quote').append(zin);
-//     }).fail(function (jqXHR, textStatus, errorThrown) {
-//         $('#quote').append("Chuck Norris just encountered his first problem, please contact us so we can improve our site.");
-//     });
-// };
-//
-//
-// var toonCategorieQuote = function (e) {
-//     e.preventDefault();
-//     $('#categorieQuote').empty();
-//     var categorieke = $('#categories').val();
-//     $.ajax({
-//         type: "GET", //-> http werkwoord
-//         url: 'https://api.chucknorris.io/jokes/random?category=' + categorieke
-//
-//     }).done(function (data, textStatus, jqXHR) {
-//         var zin = data.value;
-//         $('#categorieQuote').append("In the category ' " + categorieke + " ' we found this quote: <br> '" + zin + "'");
-//     }).fail(function (jqXHR, textStatus, errorThrown) {
-//         $('#categorieQuote').append("Chuck Norris just encountered his first problem, please contact us so we can improve our site.");
-//     });
-// };
-//
-//
-// var toonFotos = function (e) {
-//     e.preventDefault();
-//     $('#foto').empty();
-//     $('#info').empty();
-//     var zoekTerm = $('#zoekterm').val().toLowerCase();
-//     $('#opzoeking').html("U zocht op: " + zoekTerm + " Chuck Norris");
-//     $.ajax({
-//         url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f91331ed66c0eb9e256e1068014cd5fe&text=chuck+norris+" + zoekTerm + "&has_geo=1&format=json&nojsoncallback=1",
-//         dataType: "json"
-//     }).done(function (data) {
-//         var lengte = data.photos.total;
-//         var randomGetal = Math.floor((Math.random() * lengte));
-//         var foto = data.photos.photo[randomGetal].id;
-//
-//
-//
-//         $.ajax({
-//             url: "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=f91331ed66c0eb9e256e1068014cd5fe&photo_id=" + foto + "&format=json&nojsoncallback=1"
-//         }).done(function (data) {
-//             $('#foto').append("<img src=' "+ data.sizes.size[1].source + " ' alt='Chuck Norris'>");
-//         });
-//         $.ajax({
-//             url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=f91331ed66c0eb9e256e1068014cd5fe&photo_id=" + foto + "&format=json&nojsoncallback=1"
-//         }).done(function (data) {
-//             var author = data.photo.owner.realname;
-//             var titel = data.photo.title._content;
-//             var lat = data.photo.location.latitude;
-//             var long = data.photo.location.longitude;
-//             var views = data.photo.views;
-//
-//
-//             $('#info').html("Author: " + author + "<br>" + "Title: " + titel + "<br>" + "Times viewed: " + views + "<br>" + "Location: ");
-//             var locatie1 = "https://www.google.com/maps/embed/v1/place?q=" + lat + "%2C" + long + "&key=AIzaSyB2VV0_vn7reEhn-N6ULWQKB8sxJzl_-zQ";
-//             $('#locatie').attr("src", locatie1);
-//         })
-//
-//
-//     })
-//         .fail(function (jqXHR, textStatus, errorThrown) {
-//             $('#image').append("Chuck Norris just encountered his first problem, please contact us so we can improve our site.");
-//         });
-// };
-//
-//
-// $(document).ready(function () {
-//     $('#generate').on('click', toonQuote);
-//     $('#submitQuote').on('click', toonCategorieQuote);
-//     $('#submitFlickr').on('click', toonFotos);
-// });
